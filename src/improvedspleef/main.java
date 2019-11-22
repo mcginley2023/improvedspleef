5ef6c46bc051cf17289baf19dbce80f6542bc55e
mcginley2023 <carter.mcginley@kearsarge.org>
2019-11-22 08:24:25 -0500

    day 1

--- src/improvedspleef/main.java
+++ src/improvedspleef/main.java
@@ -1,12 +1,18 @@
 package improvedspleef;
 
-public class main
-{
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
-  public static void main(String[] args)
-  {
-    // TODO Auto-generated method stub
-
+public class main extends JavaPlugin implements Listener {
+  public void onEnable() {
+    getServer().getPluginManager().registerEvents(new main(), this);
   }
-
+  
+  
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent e) {
+    
+  }
 }
