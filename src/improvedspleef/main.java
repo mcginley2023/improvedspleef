cc143e953f0581ec74d479c07b5746f6e7a8f9a0
mcginley2023 <carter.mcginley@kersarge.org>
2019-11-23 10:52:07 -0500

    Day 2 - added test command

--- src/improvedspleef/main.java
+++ src/improvedspleef/main.java
@@ -1,19 +1,34 @@
 package improvedspleef;
 
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
+import org.bukkit.command.*;
+import org.bukkit.entity.*;
+import org.bukkit.event.*;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin implements Listener {
   public void onEnable() {
     getServer().getPluginManager().registerEvents(new main(), this);
+    this.getCommand("hi").setExecutor(new main());
   }
   
   
   @EventHandler
+  @Override
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	  if(sender instanceof Player) {
+		  Player p = (Player) sender;
+		  if(cmd.getName().equalsIgnoreCase("hi")) {
+			  p.sendMessage("HELLO NEW COMER");
+		  }
+		  
+	  }
+      return true;
+  }
   public void onPlayerJoin(PlayerJoinEvent e) {
     e.getPlayer().sendMessage("HELLO");
   }
+  
+  
 }
 
