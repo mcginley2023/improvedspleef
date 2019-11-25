ac9bf82e2ded37da17394a2a62a849a97602d667
mcginley2023 <carter.mcginley@kearsarge.org>
2019-11-25 08:24:29 -0500

    Day 3 - added spleef command with locations

--- src/improvedspleef/main.java
+++ src/improvedspleef/main.java
@@ -1,18 +1,31 @@
 package improvedspleef;
 
-import org.bukkit.event.EventHandler;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.Material;
+import org.bukkit.Location; 
 
 public class main extends JavaPlugin implements Listener {
   public void onEnable() {
     getServer().getPluginManager().registerEvents(new main(), this);
+    this.getCommand("spleef").setExecutor(new main());
   }
-  
-  
-  @EventHandler
-  public void onPlayerJoin(PlayerJoinEvent e) {
+  @Override
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    if(cmd.getName().equalsIgnoreCase("spleef")) {
+      Player p = (Player) sender;
+      sender.sendMessage("You typed the spleef command.");
+      World world = p.getWorld();
+      Location loc = new Location(world, 0, -1, 0);
+      Block block = world.getBlockAt(p.getLocation());
+      block.setType(Material.);
+    }
     
+      return false;
   }
 }
