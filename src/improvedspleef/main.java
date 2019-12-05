9783c5e19a76048f2dcfd567f19d50e38fa6f1fa
mcginley2023 <carter.mcginley@kearsarge.org>
2019-12-05 08:19:02 -0500

    Day 4 - basic block placing

--- src/improvedspleef/main.java
+++ src/improvedspleef/main.java
@@ -8,27 +8,40 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.Material;
-import org.bukkit.Location; 
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.EventHandler;
 
 public class main extends JavaPlugin implements Listener {
   public void onEnable() {
-    getServer().getPluginManager().registerEvents(new main(), this);
-    this.getCommand("spleef").setExecutor(new main());
+    getServer().getPluginManager().registerEvents(this, this);
+    this.getCommand("spleef").setExecutor(this);
+    
   }
   
-  
-  @EventHandler
   @Override
   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     if(cmd.getName().equalsIgnoreCase("spleef")) {
       Player p = (Player) sender;
-      sender.sendMessage("You typed the spleef command.");
-      World world = p.getWorld();
-      Location loc = new Location(world, 0, -1, 0);
-      Block block = world.getBlockAt(p.getLocation());
-      block.setType(Material.);
+      boolean place = false;
+      if(args[0].equalsIgnoreCase("on")) {
+        place = true;
+      } else if(args[0].equalsIgnoreCase("off")) {
+        place = false;
+      } else {}
     }
-    
       return false;
+  }
+  @EventHandler
+  public void onPlayerMove(PlayerMoveEvent e) {
+    Player p = (Player) e.getPlayer();
+    
+    /*if(p.getInventory().getItemInHand().getType().equals(Material.DIAMOND)) {
+      e.getFrom().add( 0 , -1 , 0 ).getBlock().setType(Material.DIAMOND_BLOCK);
+    }
+    */
+    if(p.getInventory().getItemInHand().getType().isBlock() && !p.getInventory().getItemInHand().getType().equals(Material.AIR)){
+      Material blockType = (Material) p.getInventory().getItemInHand().getType();
+      e.getFrom().add(0,-1,0).getBlock().setType(blockType);
+    }
   }
 }
